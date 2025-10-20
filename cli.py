be69d238ada9fd42ad3ef4c56344f5636d926906
Adrian Hatfield <adrianhatfield23@gmail.com>
2025-10-20 13:16:15 +0100

    Add cli and word list checking

---
+++ cli.py
@@ -0,0 +1,48 @@
+import cmd
+import pathEnnumeration
+
+"""
+- Take user input from command line
+    - Input is single URL and a .txt word list (user defined)
+- Validate the word list is real and isn't empty
+- Validate that the URL is in the correct format (.com, .net, ect.)
+
+pathCracker test_words.txt https://www.website.com/foo -> call ennumerateURL("test_words.txt", "website.com")
+
+- Display list of strings from ennumerateURL
+"""
+
+class PathCracker(cmd.Cmd):
+    def do_pathCracker(self, words, url):
+        if words and url:
+            if check_word_list(words):
+                sanitised_url = sanitise_url(url)
+                cmd.columnize(pathEnnumeration.ennumerateURL(words, sanitised_url))
+        else:
+            print("No URL and/or word list")
+    
+    def do_EOF(self, line):
+        return True
+    
+    def postloop(self):
+        print
+
+if __name__ == '__main__':
+    PathCracker().cmdloop()
+
+def sanitise_url(url: str):
+    # remove http(s)
+    # remove ://
+    # remove foo.
+    # remove /foo
+    return url
+
+def check_word_list(file: str):
+    lines = open(file, "r").readlines()
+
+    if not lines:
+        return False
+    else:
+        return True
+    
+    lines.close()
