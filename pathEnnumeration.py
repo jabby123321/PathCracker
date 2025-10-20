36c370173ffb6ea7c00046718d62de3d69053f4a
JosephStevens <jabbyste.12@gmail.com>
2025-10-20 12:50:49 +0100

    **untested made simple recusive path checking

--- pathEnnumeration.py
+++ pathEnnumeration.py
@@ -2,7 +2,17 @@ from typing import List
 
 import request
 
-""""""
+"""takes a file object for wordlist and a url"""
 def ennumerateURL(wordlist, url: str) -> List[str]:
-  raise NotImplemented
+  foundWords = []
+
+  for word in wordlist.readline():
+    match (request.checkPath(f"{url}/{word}")):
+      case 0:
+        continue
+      case 1:
+        foundWords.extend(ennumerateURL({url}/{word}))
+      case 2:
+        foundWords.append(f"{url}/{word}")
+
 
