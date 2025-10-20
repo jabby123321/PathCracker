9b3384afe277c74c96c2767fe8e4134346180496
Cicadin <newman0210@outlook.com>
2025-10-20 12:56:29 +0100

    **untested http status code to int convert

--- request.py
+++ request.py
@@ -1,9 +1,22 @@
+import requests
+
 """
-Takes a url and determines wether path exists or does not exitst
-returns 0 nothing
-        1 for directory
-        2 for valid endpoint
+Takes a url and determines whether path exists or does not exist
+  returns 0 = nothing // 404
+          1 = for directory // 301
+          2 = for valid endpoint // 200
 """
+
 def checkPath(endpoint: str) -> int:
-  raise NotImplemented
+  """converts endpoint to an int based on its status code"""
+  
+  httpGet = requests.get(f'{endpoint}') # gets the URL from the endpoint string
+
+  match httpGet.status_code: # gets status code
+    case 404: # if not present
+      return 0
+    case 301: # if directory
+      return 1
+    case 200: # if found
+      return 2
 
