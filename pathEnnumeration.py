42c814207856e492af1309eefdd0d3314ac471ee
JosephStevens <jabbyste.12@gmail.com>
2025-10-20 12:18:14 +0100

    defining internal API

---
+++ pathEnnumeration.py
@@ -0,0 +1,8 @@
+from typing import List
+
+import request
+
+""""""
+def ennumerateURL(wordlist, url: str) -> List[str]:
+  raise NotImplemented
+
---
+++ request.py
@@ -0,0 +1,9 @@
+"""
+Takes a url and determines wether path exists or does not exitst
+returns 0 nothing
+        1 for directory
+        2 for valid endpoint
+"""
+def checkPath(endpoint: str) -> int:
+  raise NotImplemented
+
